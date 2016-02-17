b32cd90068262e7468bf3ed36109737b213944ce
Oliver Gutierrez <ogutierrez@redhat.com>
2016-02-17 10:57:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# flaskless
+
+Flaskless is a python module that emulates some of the original flask features.
+
+It has been developed for flask replacement in initial [Fleet Commander](https://github.com/fleet-commander/fc-admin "Fleet Commander") release.
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="flaskless",
+    version='0.1',
+    description='Flask like python module',
+    author="Oliver Gutierrez",
+    author_email="ogutsua@gmail.com",
+    packages =['flaskless'],
+    platforms=['any'],
+)
